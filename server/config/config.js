6ea55733eee8c543a856b0e62255bbce9eddcf10
boris <brmiranda009@gmail.com>
2020-05-25 16:37:43 -0600

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe ';
 } else {
-    urlBD = 'mongodb+srv://usuario:usuario@cluster0-mwhbr.mongodb.net/cafe?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlBD;
